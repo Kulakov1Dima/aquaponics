047ff1325603851da8eca08e707f47e5d9a26514
interpreter-It <kulakovdima19@gmail.com>
2021-07-08 02:39:10 +0300

    Create Configuration.h

---
+++ aquaponics2021/Configuration.h
@@ -0,0 +1,11 @@
+#define version "0.0.5"
+#define dat "12.06.2021      22:54"
+
+#define pump1 9
+#define pump2 10
+#define pump3 11
+#define pump4 12
+
+#define FLOWSENSORPIN1 2 // пин для датчика расхода
+
+#define FLOWSENSORPIN2 3 // пин для датчика расхода
